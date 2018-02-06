638f6f10441a83929fe2f3231ff5f2ec162f8139
Caleb Hamilton <cjlhamilton@gmail.com>
2018-02-06 16:45:57 +0000

    Updated filepath

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,5 @@ set fish_greeting ""
 
 ulimit -c 0
 
-alias ct "python3 /home/caleb/notes/ctasks/ctasks.py"
+alias ct "python3 /home/caleb/scripts/ctasks/ctasks.py"
 alias assignments "python3 /home/caleb/scripts/assignments.py"
