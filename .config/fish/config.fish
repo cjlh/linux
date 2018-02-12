6e7dfe047efb7c3a9eb00d834bca3c1a523b5e39
Caleb Hamilton <cjlhamilton@gmail.com>
2018-02-12 13:11:29 +0000

    Added alias for date-tracker script

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,4 +4,5 @@ ulimit -c 0
 
 alias ct "python3 /home/caleb/scripts/ctasks/ctasks.py"
 alias assignments "python3 /home/caleb/scripts/assignments.py"
+alias dates "python3 /home/caleb/scripts/date-tracker/dates.py"
 alias spotify-lyrics "python /home/caleb/scripts/spotify-lyrics/spotify-lyrics.py"
