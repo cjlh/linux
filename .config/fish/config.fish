d669289302ffeb51fb12ddb6705ab31a983237e1
Caleb Hamilton <cjlhamilton@gmail.com>
2018-07-10 18:47:26 +0100

    Added useful ls alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,3 +6,5 @@ alias ct "python3 /home/caleb/scripts/ctasks/ctasks.py"
 alias assignments "python3 /home/caleb/scripts/assignments.py"
 alias dates "python3 /home/caleb/scripts/date-tracker/dates.py"
 alias spotify-lyrics "python /home/caleb/scripts/spotify-lyrics/spotify-lyrics.py"
+
+alias l "ls -GghB --group-directories-first"
